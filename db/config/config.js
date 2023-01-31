9551f101b41cbcc2b3db4111a9e18fe4c7dd8311
ben <7rubengarcia7@gmail.com>
2023-01-31 13:33:57 -0800

    chore: add script to migrate the db

--- db/config/config.js
+++ db/config/config.js
@@ -2,15 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // dialect: process.env.DB_DIALECT,
-    username: 'ben',
-    password: 'ben',
-    database: 'orgello_development',
-    dialect: 'postgres',
+    username: process.env.DB_USERNAME || 'ben',
+    password: process.env.DB_PASSWORD || 'ben',
+    database: process.env.DB_NAME || 'orgello_development',
+    dialect: process.env.DB_DIALECT || 'postgres',
   },
   test: {
     dialect: 'postgres',
